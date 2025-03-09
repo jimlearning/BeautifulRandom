e6aea86ffb1c830991c3af1ad0123de77680836a
jimlearning <waz0820@gmail.com>
2025-03-09 16:42:12 +0800

    Initial Commit

---
+++ BeautifulRandom/BeautifulRandom.h
@@ -0,0 +1,18 @@
+//
+//  BeautifulRandom.h
+//  BeautifulRandom
+//
+//  Created by Jim Learning on 2025/3/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BeautifulRandom.
+FOUNDATION_EXPORT double BeautifulRandomVersionNumber;
+
+//! Project version string for BeautifulRandom.
+FOUNDATION_EXPORT const unsigned char BeautifulRandomVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BeautifulRandom/PublicHeader.h>
+
+
